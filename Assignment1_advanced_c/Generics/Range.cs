83c01d0e23f9181b8dfeb48b28a586d81fb1d066
moomen mohammmed <monm73530@gmail.com>
2025-01-21 12:47:22 +0200

    Create a generic class named Range<T>

---
+++ Assignment1_advanced_c/Generics/Range.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_advanced_c.Generics
+{
+    internal class Rangey
+    {
+        //Create a generic class named Range <T> where T represents the type of values.
+        public static void Range<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+
+    }
+}
